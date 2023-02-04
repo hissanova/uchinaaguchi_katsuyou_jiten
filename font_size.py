10962a2926394230d31adc1320a4e5fbc4f6fba7
Hisanobu Nakamura <hisanobu@pop-os.localdomain>
2023-02-05 00:47:05 +0900

    still not working

--- font_size.py
+++ font_size.py
@@ -1,3 +1,6 @@
+import re
+
+
 import pdfminer
 from pdfminer.high_level import extract_pages
 from pdfminer.layout import LTTextContainer, LTChar, LTLine, LAParams
@@ -6,12 +9,39 @@ path = r'./20210312Uchinaaguchi_e.pdf'
 
 Extract_Data = []
 
-for page_layout in extract_pages(path):
-    for element in page_layout:
-        if isinstance(element, LTTextContainer):
-            font_sizes = []
-            for text_line in element:
-                for character in text_line:
-                    if isinstance(character, LTChar):
-                        font_sizes.append(character.size)
-                        Extract_Data.append([font_sizes, element.get_text()])
+for i, page_layout in enumerate(extract_pages(path)):
+    if 17 <= i <= 577:
+        # print(page_layout)
+        for j, element in enumerate(page_layout):
+            # print(j, element)
+            if isinstance(element, LTTextContainer):
+                font_sizes = []
+                for text_line in element:
+                    for character in text_line:
+                        # print(character, end="")
+                        if isinstance(character, LTChar):
+                            font_sizes.append(round(character.size, 2))
+                Extract_Data.append([font_sizes, element.get_text()])
+    # if 18 < i:
+    #     break
+
+
+# print(len(Extract_Data))
+indices = []
+for line in Extract_Data:
+    # print(len(line[0]), len(line[1]))
+    # print(line[1])
+    refined = re.sub('[\n\ ]', '', line[1])
+    # print(len(refined), refined)
+    # print(line)
+    index = ""
+    for size, char in zip(line[0], refined):
+        if 20 > size > 10:
+            index += char
+    if index:
+        indices.append((index, line[1]))
+
+with open("index_words.txt", 'w') as fp:
+    for index, line in indices:
+        line = line.replace('\n', '')
+        fp.write(f"{index}\t{line}\n")
--- from_pdf_to_json.py
+++ from_pdf_to_json.py
@@ -1,4 +1,6 @@
 import json
+import re
+from functools import reduce
 from pprint import pprint
 
 
@@ -8,6 +10,14 @@ chapters = [{"title": "名詞･動詞編", "range": [17, 413]},
             {"title": "形容詞編", "range": [415, 495]},
             {"title": "副詞編", "range": [497, 577]},
             ]
+
+with open("./index_words.txt") as indices_file:
+    indices = {}
+    for row in indices_file:
+        index, line = row.split("\t")
+        indices[line.strip("\n")] = index
+
+
 
 # creating a pdf file object
 with open('20210312Uchinaaguchi_e.pdf', 'rb') as pdfFileObj:
@@ -28,28 +38,43 @@ with open('20210312Uchinaaguchi_e.pdf', 'rb') as pdfFileObj:
             pages.append(pageObj.extractText())
         return pages
 
+    def is_index_in_tail(line):
+        return indices.get(line.replace("\n", ""), False)
     items = []
     current_entry = ""
     current_contents = ""
-    for page_text in get_pages(17, 577):
+    in_definition = False
+    for page_text in get_pages(17, 200):
+        # print(page_text)
         lines = page_text.split("\n")
         # lines[1: -1] なのは、ページタイトルとページ数を除くため
         for i, line in enumerate(lines[1:-1]):
-            if "〈" in line:
+            index = is_index_in_tail(line)
+            # print(index)
+            if index:
+                pattern = '(' + index + ' *〈[\w（）、]+〉? *)'
+                split_contents = re.split(pattern, line)
+                print(split_contents)
+                tail_of_current_ctnts = split_contents.pop(0)
+                next_entry = split_contents.pop(0)
+                head_of_next_ctnts = split_contents.pop(0)
                 if current_contents:
-                    items.append({"entry": current_entry, "contents": current_contents})
-                if "〉" in line:
-                    closing_pos = line.index("〉") + 1
-                    current_entry = line[:closing_pos]
-                    current_contents = line[closing_pos:]
-                else:
-                    current_entry = line
-                    current_contents = ""
+                    items.append({"entry": current_entry,
+                                  "contents": current_contents + tail_of_current_ctnts})
+                    print(items[-1])
+                current_entry = next_entry
+                current_contents = head_of_next_ctnts
+                if len(head_of_next_ctnts) == 0:
+                    in_definition = True
             else:
-                if "〉" in line:
-                    closing_pos = line.index("〉") + 1
-                    current_entry += line[:closing_pos]
-                    current_contents = line[closing_pos:]
+                if in_definition:
+                    if "〉" in line:
+                        closing_pos = line.index("〉") + 1
+                        current_entry += line[:closing_pos]
+                        current_contents = line[closing_pos:]
+                        in_definition = False
+                    else:
+                        current_entry += line
                 else:
                     current_contents += line
 
