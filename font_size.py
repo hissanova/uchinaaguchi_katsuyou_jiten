bab929e57010234ddd6646499ab207be06fad3f7
Hisanobu Nakamura <hisanobu@pop-os.localdomain>
2023-03-04 15:52:10 +0900

    new pdf reader

--- font_size.py
+++ font_size.py
@@ -1,47 +1,33 @@
-import re
+from itertools import islice
+from collections import Counter
 
-
-import pdfminer
 from pdfminer.high_level import extract_pages
-from pdfminer.layout import LTTextContainer, LTChar, LTLine, LAParams
+from pdfminer.layout import LTTextBox, LTTextContainer, LTChar, LTLine
 
 path = r'./20210312Uchinaaguchi_e.pdf'
+page_start = 17
+page_end = 577
 
-Extract_Data = []
+h_coord_counter = Counter()
+height_counter = Counter()
+for page_layout in islice(extract_pages(path), 17, 577):
+    left_col = []
+    right_col = []
+    for h_box in islice(page_layout, 1, None):
+        # print(h_box)
+        # print(h_box.x0, h_box.x1 - h_box.x0, h_box.y1 - h_box.y0)
+        if isinstance(h_box, LTTextContainer):
+            head_x_coord = round(h_box.x0)
+            height = round(h_box.height, 1)
+            line = (head_x_coord, height, h_box.get_text())
+            h_coord_counter.update([head_x_coord])
+            height_counter.update([height])
+            if head_x_coord < 100:
+                left_col.append(line)
+            else:
+                right_col.append(line)
+    # for line in left_col + right_col:
+    #     print(line)
 
-for i, page_layout in enumerate(extract_pages(path)):
-    if 17 <= i <= 577:
-        # print(page_layout)
-        for j, element in enumerate(page_layout):
-            # print(j, element)
-            if isinstance(element, LTTextContainer):
-                font_sizes = []
-                for text_line in element:
-                    for character in text_line:
-                        # print(character, end="")
-                        if isinstance(character, LTChar):
-                            font_sizes.append(round(character.size, 2))
-                Extract_Data.append([font_sizes, element.get_text()])
-    # if 18 < i:
-    #     break
-
-
-# print(len(Extract_Data))
-indices = []
-for line in Extract_Data:
-    # print(len(line[0]), len(line[1]))
-    # print(line[1])
-    refined = re.sub('[\n\ ]', '', line[1])
-    # print(len(refined), refined)
-    # print(line)
-    index = ""
-    for size, char in zip(line[0], refined):
-        if 20 > size > 10:
-            index += char
-    if index:
-        indices.append((index, line[1]))
-
-with open("index_words.txt", 'w') as fp:
-    for index, line in indices:
-        line = line.replace('\n', '')
-        fp.write(f"{index}\t{line}\n")
+print(sorted(h_coord_counter.items()))
+print(sorted(height_counter.items()))
