fee2ba7685a37c933228f57e92ce2ed491b59374
Hisanobu Nakamura <hisanobu@pop-os.localdomain>
2023-03-21 20:27:32 +0900

    WIP refining section contents

--- generate_dictionary.py
+++ generate_dictionary.py
@@ -5,10 +5,39 @@ import re
 # from pprint import pprint
 from typing import Dict
 
-chapters = [{"title": "名詞･動詞編", "range": [17, 413]},
-            {"title": "形容詞編", "range": [415, 495]},
-            {"title": "副詞編", "range": [497, 577]},
-            ]
+chapters = [
+    {
+        "title": "名詞･動詞編",
+        "range": [17, 413]
+    },
+    {
+        "title": "形容詞編",
+        "range": [415, 495]
+    },
+    {
+        "title": "副詞編",
+        "range": [497, 577]
+    },
+]
+
+
+def separate_variations(index):
+    index = index.replace("（", "(").replace("）", ")")
+    return list(
+        set([
+            "".join(re.split(r"\(\w+\)", index)),
+            "".join(re.split(r"\((\w+)\)", index))
+        ]))
+
+
+def split_recur(st, spl_lst):
+    if not spl_lst:
+        return separate_variations(st)
+    else:
+        return [
+            x for lis in map(lambda s: split_recur(s, spl_lst[1:]),
+                             st.split(spl_lst[0])) for x in lis
+        ]
 
 
 def is_in(val, interval) -> bool:
@@ -31,10 +60,7 @@ def add_pos(item):
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument(
-        '--format',
-        choices=['json', 'jsonl'],
-        default="json")
+    parser.add_argument('--format', choices=['json', 'jsonl'], default="json")
     return parser.parse_args()
 
 
@@ -51,7 +77,9 @@ def main():
         original_contents = item["contents"]
         # 大和口での意味、その他の内容に分割する
         split_point = original_contents.index("〉")
-        yamato, contents = original_contents[:split_point+1], original_contents[split_point+1:]
+        yamato, contents = original_contents[:split_point +
+                                             1], original_contents[
+                                                 split_point + 1:]
         item["yamato"] = yamato
         # 見出し語の位置情報によって、親の語彙エントリーの関連語リストに登録する
         index_x_coord = item["index_x_coord"]
@@ -61,34 +89,50 @@ def main():
             prev_main_entry = item
         else:
             parent_related_list = prev_main_entry.get("related", [])
-            parent_related_list.append({"index": item["index"],
-                                        "yamato": item["yamato"],
-                                        "id": i})
+            parent_related_list.append({
+                "index": item["index"].split("、"),
+                "yamato": item["yamato"],
+                "id": i
+            })
             prev_main_entry["related"] = parent_related_list
         # if not yamato.startswith("〈"):
         #     print(item['index'], yamato)
         # if len(contents) == 0:
         #     print(item)
+
         # 活用、例文、参照項に分割して、それぞれアイテムのアトリビュートとする
         split_contents = re.split(r"(【\w】)", contents)[1:]
         if len(split_contents) % 2 == 1:
             raise Exception(f"{split_contents}")
         for i in range(0, len(split_contents), 2):
             section_head = split_contents[i]
+            section = split_contents[i + 1]
             if section_head == "【活】":
-                key = "conjugation"
+
+                def decompose_verb_conj(contents):
+                    return contents
+
+                item["conjugation"] = decompose_verb_conj(section)
             elif section_head == "【例】":
-                key = "sample_setences"
+
+                def decompose_sample_sentences(contents):
+                    return contents
+
+                item["sample_sentences"] = decompose_sample_sentences(section)
             elif section_head == "【参】":
-                key = "reference"
+                item["reference"] = section
             else:
                 raise NotImplementedError
-            item[key] = section_head + split_contents[i + 1]
+
+            # item[key] = section_head + split_contents[i + 1]
         add_pos(item)
         # 最終的な辞書に要らない項目の削除
         item.pop("contents")
         item.pop("index_x_coord")
         item.pop("index_size")
+        # 複数 index が"、"で区切られてるのをリストに分割。
+        item["index"] = split_recur(item["index"].replace(" ", ""), ["、", "／"])
+        item["id"] = i
         # 辞書リストに登録
         dictionary.append(item)
 
